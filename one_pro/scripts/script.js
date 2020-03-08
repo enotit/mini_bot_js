ecf47346fa7cb3b9fcd957b0607c0ae7c78c993b
enotit <fkcman2003@gmail.com>
2020-03-09 00:45:46 +0300

    change system authorization

--- one_pro/scripts/script.js
+++ one_pro/scripts/script.js
@@ -1,16 +1,21 @@
-var sex = 0;
-var charact = '\n🤡:'
-var bot = '\n🤖: '
-let mesers = []
-var count_xz = 0;
-var name;
+// One used
+let sogl = ['да', 'согласен', 'угу', 'ага', 'дэ', 'верно']
+// Always used
+var sex = 0; // 1 - муж(3), 2 - жен(4)
+var charact = '\n🤡:' // безликий
+var bot = '\n🤖: ' // Бот
+let mesers = [] // список сообщений
+var count_xz = 0; // список вопросов без предв-го ответа
+var name = ''; // имя пользователя
+
+
 function fir(){
     text = document.getElementById('valueID').value;    // Забираем текст с input
     document.getElementById('valueID').value = '';  // input = ''
     var Area = document.getElementById("messangers"); // Обнаруживаем Area
     if(text != ''){
         mesers.push(text) // Добавляем сообщение в массив
-        Area.value += charact + text; // Добавляем  сообщение в Area
+        Area.value += charact + text; // Добавляем  сообщение в Area пользователя
         Area.value += bot + answer(text)
     }
     else{
@@ -21,14 +26,22 @@ function fir(){
     }
 }
 
+function only_russia(e) {
+    t = true
+    for(var i = 0; i < e.length; i++){
+        if(!(e.codePointAt(i) > 1039 && e.codePointAt(i) < 1104)){
+            t = false}}
+    return t;
+}
+
 function what_name(names){
     let last = names.slice(-1).toLowerCase();
-    if(last == 'р' || last == 'й' || last == 'н' || last == 'д' || last == 'с' || last == 'в' || last == 'с' || last == 'й' || names == 'Добрыня' || last == 'т'){
-        charact = '\n🕺: '
-        sex = 1
+    if(last == 'р' || last == 'й' || last == 'н' || last == 'д' || last == 'с' || last == 'в' || last == 'ж' || last == 'й' || names == 'Добрыня' || last == 'т' ){
+        charact = '\n🕺: ';
+        sex = 3;
     }else{
-        charact = '\n👩: '
-        sex = 2
+        charact = '\n👩: ';
+        sex = 4;
     }
     name = names;
     delete last;
@@ -36,34 +49,37 @@ function what_name(names){
 }
 
 function answer(quest){
-if(sex == 0){
-    if(text.split(' ').length - 1 > 0){
-        return 'Введи одним словом или же ник...';
+if(name == ''){
+    if(quest.split(' ').length - 1 > 0 || !only_russia(quest)){
+        return 'Введи одним словом или же ник, русскими буквами!';
     }
     else{
         what_name(quest)
-        return 'Оооо, привет, ' + quest + ', как твои дела?'
+        return `Система выбрала вам пол ${(sex == 3)? 'мужской': 'женский'}, верно?`
     }
+}
+else if(sex > 2){
+    console.log('заходит')
+    if(quest.toLowerCase == 'да'){
+        sex -= 2
+        return `Ваш пол выставлен ${(sex == 3)? 'мужской': 'женский'}`
+    }else{
+        sex = (sex == 4)? 2:1;
+        charact = (sex == 1)? '\n🕺: ':'\n👩: ';
+        return `Ваш пол выставлен ${(sex == 2)? 'женский': 'мужской'}`}
 }
 else{
     // Приветствие 
     if('прив' == text.slice(0,4).toLowerCase() || 'здар' == text.slice(0,4).toLowerCase() || 'здра' == text.slice(0,4).toLowerCase()){
-        Area.value += bot + 'Привет - привет!';
+        Area.value += 'Привет - привет!';
     }
     // Админ ответ
     else if('admin' == text){
-        Area.value += bot + 'Держите: [' + mesers + ']'
+        Area.value += 'Держите: [' + mesers + ']'
         }
     // Нету готового ответа
     else{
         count_xz += 1;
-        switch(count_xz){
-        case 1:
-            return 'Я пожалуй проигнорирую...';
-        case 2:
-            return name +', я надеюсь у тебя всё хорошо!)';
-        case 3:
-            count_xz = 0;
-            return 'Расскажи, как день:)';
-        }
+        var ran_ans = ['Я пожалуй проигнорирую...', `${name}, я надеюсь у тебя всё хорошо!)`, 'Расскажи, как день:)']
+        return ran_ans[Math.floor(Math.random() * Math.floor(ran_ans.length ))]
 }}}
